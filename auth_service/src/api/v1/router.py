6f4c98144fa9d7f3b7697a7eafba64f9b6878a18
ksiki <j.kolyasha@gmail.com>
2026-09-16 14:20:24 +0300

    feat: include new routers

--- auth_service/src/api/v1/router.py
+++ auth_service/src/api/v1/router.py
@@ -1,8 +1,17 @@
 from fastapi import APIRouter
 
-from src.api.v1.endpoints import recovery_router, registration_router
+from src.api.v1.endpoints import (
+    auth_router,
+    oauth_router,
+    recovery_router,
+    registration_router,
+    users_router,
+)
 
 router = APIRouter(prefix="/v1")
 
 router.include_router(registration_router)
 router.include_router(recovery_router)
+router.include_router(auth_router)
+router.include_router(oauth_router)
+router.include_router(users_router)
